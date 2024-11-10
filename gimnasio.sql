3230278bf334bb41824c24c924518d55f2070578
ivanleirbag <ivanpeceto20@gmail.com>
2024-11-10 13:37:05 -0300

    SUBI LA BASE PQ ME OLVIDE ANTES

--- gimnasio.sql
+++ gimnasio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 08-11-2024 a las 04:19:19
+-- Tiempo de generación: 10-11-2024 a las 17:35:46
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,22 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `clasesgrupales`
---
-
-CREATE TABLE `clasesgrupales` (
-  `id_clase` int(11) NOT NULL,
-  `cod_clase` int(11) NOT NULL,
-  `capMax_clase` tinyint(4) NOT NULL,
-  `id_nombreClase` int(11) NOT NULL,
-  `id_horario` int(11) NOT NULL,
-  `id_entrenador` int(11) NOT NULL,
-  `id_ubicacion` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `clientes`
 --
 
@@ -49,40 +33,22 @@ CREATE TABLE `clientes` (
   `apellido_cliente` varchar(50) NOT NULL,
   `dni_cliente` int(9) NOT NULL,
   `fechaNac_cliente` date NOT NULL,
+  `direccion_cliente` varchar(50) NOT NULL,
   `telefono_cliente` int(11) NOT NULL,
-  `email_cliente` int(11) NOT NULL,
+  `email_cliente` varchar(50) NOT NULL,
   `fechaIns_cliente` date NOT NULL,
-  `id_clase` int(11) NOT NULL,
-  `id_direccion` int(11) NOT NULL,
   `id_plan` int(11) NOT NULL,
-  `id_estado` int(11) NOT NULL,
-  `fecha_inicio` int(11) NOT NULL
+  `id_estado_memb` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `direccion`
+-- Volcado de datos para la tabla `clientes`
 --
 
-CREATE TABLE `direccion` (
-  `id_direccion` int(11) NOT NULL,
-  `calle_direccion` varchar(50) NOT NULL,
-  `cp_direccion` varchar(10) NOT NULL,
-  `ciudad_direccion` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `duracion`
---
-
-CREATE TABLE `duracion` (
-  `id_duracion` int(11) NOT NULL,
-  `valor_duracion` time NOT NULL,
-  `descrip_duracion` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `clientes` (`id_cliente`, `nombre_cliente`, `apellido_cliente`, `dni_cliente`, `fechaNac_cliente`, `direccion_cliente`, `telefono_cliente`, `email_cliente`, `fechaIns_cliente`, `id_plan`, `id_estado_memb`) VALUES
+(1, 'EDITADO', 'EDITADO PRIMER EDIT', 44555666, '1999-01-01', 'DIRECCION EDITADA', 1111111, 'editado@primeredit.com', '2024-01-01', 1, 1),
+(2, 'Alberto', 'Example', 44555666, '2000-02-01', 'Calle Nunca Viva 321', 3333444, 'example@client.com', '2024-01-02', 1, 1),
+(3, 'Pedro', 'Primeragregar', 33444555, '2003-02-03', 'Calle al lado de siempre viva 111', 4449955, 'hola@quetal.com', '2024-02-03', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -102,6 +68,13 @@ CREATE TABLE `entrenadores` (
   `id_especialidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `entrenadores`
+--
+
+INSERT INTO `entrenadores` (`id_entrenador`, `nombre_entrenador`, `apellido_entrenador`, `dni_entrenador`, `fechaContr_entrenador`, `email_entrenador`, `telefono_entrenador`, `id_estado`, `id_especialidad`) VALUES
+(1, 'Pepetrainer', 'Pesoslibres', 48492444, '2024-02-01', 'trainer@pesas.com', 22334, 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -114,55 +87,72 @@ CREATE TABLE `especialidades` (
   `descrip_especialidad` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `estado`
+-- Volcado de datos para la tabla `especialidades`
 --
 
-CREATE TABLE `estado` (
-  `id_estado` int(11) NOT NULL,
-  `status_estado` tinyint(1) NOT NULL,
-  `descrip_estado` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `especialidades` (`id_especialidad`, `nombre_especialidad`, `descrip_especialidad`) VALUES
+(1, 'Body Building', 'Especialidad centrada en la hipertrofia muscular.');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `fechainicio`
+-- Estructura de tabla para la tabla `estados_entrenadores`
 --
 
-CREATE TABLE `fechainicio` (
-  `id_fechainicio` int(11) NOT NULL,
-  `fechainicio_fecha` int(11) NOT NULL,
-  `id_cliente` int(11) NOT NULL,
-  `id_clase` int(11) NOT NULL
+CREATE TABLE `estados_entrenadores` (
+  `id_estado_ent` int(11) NOT NULL,
+  `estado_ent` varchar(20) NOT NULL,
+  `desc_estado_ent` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estados_entrenadores`
+--
+
+INSERT INTO `estados_entrenadores` (`id_estado_ent`, `estado_ent`, `desc_estado_ent`) VALUES
+(1, 'Activo', 'Al dia de la fecha, el entrenador imparte clases'),
+(2, 'Inactivo', 'Al dia de la fecha, el entrenador no imparte clases');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `horarios`
+-- Estructura de tabla para la tabla `estados_membresias`
 --
 
-CREATE TABLE `horarios` (
-  `id_horario` int(11) NOT NULL,
-  `fecha_horario` datetime NOT NULL
+CREATE TABLE `estados_membresias` (
+  `id_estado_memb` int(11) NOT NULL,
+  `estado_memb` varchar(20) NOT NULL,
+  `desc_estado_memb` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estados_membresias`
+--
+
+INSERT INTO `estados_membresias` (`id_estado_memb`, `estado_memb`, `desc_estado_memb`) VALUES
+(1, 'Activo', 'El cliente esta habilitado a usar las instalaciones'),
+(2, 'Inactivo', 'El cliente no esta habilitado a usar las instalaciones');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `instalacionesuso`
+-- Estructura de tabla para la tabla `estados_pago`
 --
 
-CREATE TABLE `instalacionesuso` (
-  `id_instUso` int(11) NOT NULL,
-  `id_horario` int(11) NOT NULL,
-  `id_ubicacion` int(11) NOT NULL,
-  `id_duracion` int(11) NOT NULL,
-  `id_cliente` int(11) NOT NULL
+CREATE TABLE `estados_pago` (
+  `id_estado_pago` int(11) NOT NULL,
+  `estado_pago` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estados_pago`
+--
+
+INSERT INTO `estados_pago` (`id_estado_pago`, `estado_pago`) VALUES
+(1, 'Realizado'),
+(2, 'Denegado'),
+(3, 'En espera');
 
 -- --------------------------------------------------------
 
@@ -176,28 +166,15 @@ CREATE TABLE `metodopago` (
   `descrip_metodoPago` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `nombreclase`
+-- Volcado de datos para la tabla `metodopago`
 --
 
-CREATE TABLE `nombreclase` (
-  `id_nombreClase` int(11) NOT NULL,
-  `titulo_nombreClase` varchar(50) NOT NULL,
-  `descrip_nombreClase` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `nombreplan`
---
-
-CREATE TABLE `nombreplan` (
-  `id_nombrePlan` int(11) NOT NULL,
-  `titulo_nombrePlan` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `metodopago` (`id_metodoPago`, `nombre_metodoPago`, `descrip_metodoPago`) VALUES
+(1, 'Débito', 'Tarjetas de débito'),
+(2, 'Crédito', 'Tarjetas de crédito'),
+(3, 'Transferencia', 'Transferencias bancarias'),
+(4, 'Efectivo', 'Dinero en efectivo');
 
 -- --------------------------------------------------------
 
@@ -208,12 +185,19 @@ CREATE TABLE `nombreplan` (
 CREATE TABLE `pagos` (
   `id_pago` int(11) NOT NULL,
   `fecha_pago` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `monto_pago` int(11) NOT NULL,
+  `monto_pago` decimal(10,2) NOT NULL,
   `id_cliente` int(11) NOT NULL,
   `id_clase` int(11) NOT NULL,
   `id_estado` int(11) NOT NULL,
   `id_metodopago` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pagos`
+--
+
+INSERT INTO `pagos` (`id_pago`, `fecha_pago`, `monto_pago`, `id_cliente`, `id_clase`, `id_estado`, `id_metodopago`) VALUES
+(1, '2024-11-08 21:13:36', 1000.00, 1, 1, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -226,22 +210,17 @@ CREATE TABLE `plan` (
   `descrip_plan` text NOT NULL,
   `cod_plan` int(11) NOT NULL,
   `duracion_plan` tinyint(4) NOT NULL,
-  `cantSesion_plan` int(11) NOT NULL,
-  `id_nombrePlan` int(11) NOT NULL,
+  `sesiones_semanales_plan` int(11) NOT NULL,
+  `nombre_plan` varchar(50) NOT NULL,
   `id_entrenador` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `ubicacion`
+-- Volcado de datos para la tabla `plan`
 --
 
-CREATE TABLE `ubicacion` (
-  `id_ubicacion` int(11) NOT NULL,
-  `lugar_ubicacion` varchar(50) NOT NULL,
-  `descrip_ubicacion` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `plan` (`id_plan`, `descrip_plan`, `cod_plan`, `duracion_plan`, `sesiones_semanales_plan`, `nombre_plan`, `id_entrenador`) VALUES
+(1, 'Centrado en rutinas báscias para los principiantes', 10, 4, 3, 'Musculación básica', 1);
 
 -- --------------------------------------------------------
 
@@ -268,37 +247,12 @@ INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `email_usuario`, `passwo
 --
 
 --
--- Indices de la tabla `clasesgrupales`
---
-ALTER TABLE `clasesgrupales`
-  ADD PRIMARY KEY (`id_clase`),
-  ADD KEY `nombreClaseid` (`id_nombreClase`),
-  ADD KEY `identrenador` (`id_entrenador`),
-  ADD KEY `idhorario` (`id_horario`),
-  ADD KEY `idubicacion` (`id_ubicacion`);
-
---
 -- Indices de la tabla `clientes`
 --
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id_cliente`),
-  ADD KEY `direccionid` (`id_direccion`),
   ADD KEY `planid` (`id_plan`),
-  ADD KEY `estadoid` (`id_estado`),
-  ADD KEY `idclas` (`id_clase`),
-  ADD KEY `fechain` (`fecha_inicio`);
-
---
--- Indices de la tabla `direccion`
---
-ALTER TABLE `direccion`
-  ADD PRIMARY KEY (`id_direccion`);
-
---
--- Indices de la tabla `duracion`
---
-ALTER TABLE `duracion`
-  ADD PRIMARY KEY (`id_duracion`);
+  ADD KEY `estadoid` (`id_estado_memb`);
 
 --
 -- Indices de la tabla `entrenadores`
@@ -315,52 +269,28 @@ ALTER TABLE `especialidades`
   ADD PRIMARY KEY (`id_especialidad`);
 
 --
--- Indices de la tabla `estado`
+-- Indices de la tabla `estados_entrenadores`
 --
-ALTER TABLE `estado`
-  ADD PRIMARY KEY (`id_estado`);
+ALTER TABLE `estados_entrenadores`
+  ADD PRIMARY KEY (`id_estado_ent`);
 
 --
--- Indices de la tabla `fechainicio`
+-- Indices de la tabla `estados_membresias`
 --
-ALTER TABLE `fechainicio`
-  ADD PRIMARY KEY (`id_fechainicio`),
-  ADD KEY `idcli` (`id_cliente`),
-  ADD KEY `idclass` (`id_clase`);
+ALTER TABLE `estados_membresias`
+  ADD PRIMARY KEY (`id_estado_memb`);
 
 --
--- Indices de la tabla `horarios`
+-- Indices de la tabla `estados_pago`
 --
-ALTER TABLE `horarios`
-  ADD PRIMARY KEY (`id_horario`);
-
---
--- Indices de la tabla `instalacionesuso`
---
-ALTER TABLE `instalacionesuso`
-  ADD PRIMARY KEY (`id_instUso`),
-  ADD KEY `idhorarioentrada` (`id_horario`),
-  ADD KEY `ubicid` (`id_ubicacion`),
-  ADD KEY `duracionid` (`id_duracion`),
-  ADD KEY `clienteid` (`id_cliente`);
+ALTER TABLE `estados_pago`
+  ADD PRIMARY KEY (`id_estado_pago`);
 
 --
 -- Indices de la tabla `metodopago`
 --
 ALTER TABLE `metodopago`
   ADD PRIMARY KEY (`id_metodoPago`);
-
---
--- Indices de la tabla `nombreclase`
---
-ALTER TABLE `nombreclase`
-  ADD PRIMARY KEY (`id_nombreClase`);
-
---
--- Indices de la tabla `nombreplan`
---
-ALTER TABLE `nombreplan`
-  ADD PRIMARY KEY (`id_nombrePlan`);
 
 --
 -- Indices de la tabla `pagos`
@@ -377,14 +307,8 @@ ALTER TABLE `pagos`
 --
 ALTER TABLE `plan`
   ADD PRIMARY KEY (`id_plan`),
-  ADD KEY `nombreplanid` (`id_nombrePlan`),
+  ADD UNIQUE KEY `cod_plan` (`cod_plan`),
   ADD KEY `entrenadorid` (`id_entrenador`);
-
---
--- Indices de la tabla `ubicacion`
---
-ALTER TABLE `ubicacion`
-  ADD PRIMARY KEY (`id_ubicacion`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -394,25 +318,78 @@ ALTER TABLE `usuarios`
   ADD UNIQUE KEY `email_usuario` (`email_usuario`);
 
 --
--- Restricciones para tablas volcadas
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- Filtros para la tabla `clasesgrupales`
+-- AUTO_INCREMENT de la tabla `clientes`
 --
-ALTER TABLE `clasesgrupales`
-  ADD CONSTRAINT `identrenador` FOREIGN KEY (`id_entrenador`) REFERENCES `entrenadores` (`id_entrenador`),
-  ADD CONSTRAINT `idhorario` FOREIGN KEY (`id_horario`) REFERENCES `horarios` (`id_horario`),
-  ADD CONSTRAINT `idubicacion` FOREIGN KEY (`id_ubicacion`) REFERENCES `ubicacion` (`id_ubicacion`),
-  ADD CONSTRAINT `nombreClaseid` FOREIGN KEY (`id_nombreClase`) REFERENCES `nombreclase` (`id_nombreClase`);
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `entrenadores`
+--
+ALTER TABLE `entrenadores`
+  MODIFY `id_entrenador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `especialidades`
+--
+ALTER TABLE `especialidades`
+  MODIFY `id_especialidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `estados_entrenadores`
+--
+ALTER TABLE `estados_entrenadores`
+  MODIFY `id_estado_ent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `estados_membresias`
+--
+ALTER TABLE `estados_membresias`
+  MODIFY `id_estado_memb` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `estados_pago`
+--
+ALTER TABLE `estados_pago`
+  MODIFY `id_estado_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `metodopago`
+--
+ALTER TABLE `metodopago`
+  MODIFY `id_metodoPago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  MODIFY `id_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `plan`
+--
+ALTER TABLE `plan`
+  MODIFY `id_plan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
 
 --
 -- Filtros para la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  ADD CONSTRAINT `direccionid` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id_direccion`),
-  ADD CONSTRAINT `estadoid` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`),
-  ADD CONSTRAINT `fechain` FOREIGN KEY (`fecha_inicio`) REFERENCES `fechainicio` (`id_fechainicio`),
+  ADD CONSTRAINT `estadoid` FOREIGN KEY (`id_estado_memb`) REFERENCES `estados_membresias` (`id_estado_memb`),
   ADD CONSTRAINT `planid` FOREIGN KEY (`id_plan`) REFERENCES `plan` (`id_plan`);
 
 --
@@ -420,39 +397,21 @@ ALTER TABLE `clientes`
 --
 ALTER TABLE `entrenadores`
   ADD CONSTRAINT `especialidadid` FOREIGN KEY (`id_especialidad`) REFERENCES `especialidades` (`id_especialidad`),
-  ADD CONSTRAINT `estid` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`);
-
---
--- Filtros para la tabla `fechainicio`
---
-ALTER TABLE `fechainicio`
-  ADD CONSTRAINT `idclass` FOREIGN KEY (`id_clase`) REFERENCES `clasesgrupales` (`id_clase`),
-  ADD CONSTRAINT `idcli` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`);
-
---
--- Filtros para la tabla `instalacionesuso`
---
-ALTER TABLE `instalacionesuso`
-  ADD CONSTRAINT `clienteid` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
-  ADD CONSTRAINT `duracionid` FOREIGN KEY (`id_duracion`) REFERENCES `duracion` (`id_duracion`),
-  ADD CONSTRAINT `idhorarioentrada` FOREIGN KEY (`id_horario`) REFERENCES `horarios` (`id_horario`),
-  ADD CONSTRAINT `ubicid` FOREIGN KEY (`id_ubicacion`) REFERENCES `ubicacion` (`id_ubicacion`);
+  ADD CONSTRAINT `estid` FOREIGN KEY (`id_entrenador`) REFERENCES `estados_entrenadores` (`id_estado_ent`);
 
 --
 -- Filtros para la tabla `pagos`
 --
 ALTER TABLE `pagos`
-  ADD CONSTRAINT `idclase` FOREIGN KEY (`id_clase`) REFERENCES `clasesgrupales` (`id_clase`),
   ADD CONSTRAINT `idcliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
   ADD CONSTRAINT `idmetodopago` FOREIGN KEY (`id_metodopago`) REFERENCES `metodopago` (`id_metodoPago`),
-  ADD CONSTRAINT `statusid` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`);
+  ADD CONSTRAINT `statusid` FOREIGN KEY (`id_estado`) REFERENCES `estados_membresias` (`id_estado_memb`);
 
 --
 -- Filtros para la tabla `plan`
 --
 ALTER TABLE `plan`
-  ADD CONSTRAINT `entrenadorid` FOREIGN KEY (`id_entrenador`) REFERENCES `entrenadores` (`id_entrenador`),
-  ADD CONSTRAINT `nombreplanid` FOREIGN KEY (`id_nombrePlan`) REFERENCES `nombreplan` (`id_nombrePlan`);
+  ADD CONSTRAINT `entrenadorid` FOREIGN KEY (`id_entrenador`) REFERENCES `entrenadores` (`id_entrenador`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
